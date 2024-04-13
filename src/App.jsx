3ae92e008aa4bac3a0da21d1c61f5472c8d36904
carlos <giovannivalverde@hotmail.com>
2024-04-13 08:17:52 -0400

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,22 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import { Button } from './components/Button'
-import { WapperList } from './components/WrapperList'
-import { List } from './components/List'
+//import { WapperList } from './components/WrapperList'
+//import { List } from './components/List'
 import './App.css'
 
 function App() {
+  const buttonText = "Count: ";
+  return (
+    <>
+      <h1>HOLA DESDE VITE</h1>
+      <Button text={buttonText}/>
+    </>
+  )
+}
+export default App
+
+/*function App() {
 
   const buttonText = "Count : ";
   return (
@@ -18,4 +29,4 @@ function App() {
   )
 }
 
-export default App
+export default App*/
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,24 @@
 import { useState } from "react";
 
+export const Button = (props) => {
+    const { text } = props;
+    const [count, setCount] = useState(0);
+
+    function handlerButton() {
+        const randomNumber = Math.floor(Math.random() * 100) + 1;
+        alert(randomNumber);
+        setCount(count + 1);
+    }
+
+    return (
+        <button onClick={handlerButton}>
+            {text} {count}
+        </button>
+    );
+}
+
+/*import { useState } from "react";
+
 export const Button = props => {
     const { text } = props;
     const[count, setCount] = useState(0);
@@ -12,4 +31,4 @@ export const Button = props => {
             { text } { count }
         </button>
     );
-}
+}*/
