c59eb34993d202b9d0643e9eec1e63d717b7c59f
carlos <giovannivalverde@hotmail.com>
2024-04-13 08:44:28 -0400

    fourth commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import { Button } from './components/Button'
-import { WapperList } from './components/WrapperList'
-import { List } from './components/List'
+//import { Button } from './components/Button'//
+//import { WapperList } from './components/WrapperList'//
+//import { List } from './components/List'//
+
 import './App.css'
 
 function App() {
 
-  const buttonText = "Count : ";
+  const buttonText = "Count: ";
   return (
     <>
-      <WapperList>
-        <List/>
-      </WapperList>
+      <h1>HOLA DESDE VITE</h1>
+      <Button text={buttonText}/>
     </>
   )
 }
--- src/components/RandomComponent.jsx
+++ src/components/RandomComponent.jsx
@@ -1,21 +1,5 @@
 import { useState } from "react";
 
-export const Button = props => {
-    const { text } = props;
-    const[count, setCount] = useState(0);
-    function handlerButton(){
-        setCount(count + 5);
-    }
-
-    return (
-        <button onClick={ handlerButton }>
-            { text } { count }
-        </button>
-    );
-}
-
-/*import { useState } from "react";
-
 export const Button = (props) => {
     const { text } = props;
     const [count, setCount] = useState(0);
@@ -36,4 +20,4 @@ export const Button = (props) => {
             <button onClick={handleReset}>Resetear</button>
         </div>
     );
-}*/
+}
